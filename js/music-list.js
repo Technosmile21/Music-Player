26f6638871551148870486fb0a74f99cf7298b83
Ankit Maddheshiya <technosmile21@gmail.com>
2023-12-02 23:14:48 +0530

    music player by Ankit

---
+++ js/music-list.js
@@ -0,0 +1,68 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tu Hi Haqeeqat",
+    artist: "Emraan Hashmi",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Janiye",
+    artist: "Vishal Mishra|Rashmeet Kaur",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tu Mera Koi Hoke Bhi Kuch Lage",
+    artist: "Arijit Singh|Vanun Kriti",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Tum Hi Aana",
+    artist: "Payal Dev|Kunaal V",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tohre Me Base Raja Humro Paranwa Ho",
+    artist: "Swati Mishra",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Aasmano Pe Jo Khuda Hai",
+    artist: "Jubin N|Tanishk B",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Pehli Nazar Mein",
+    artist: "Atif Aslam",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Hum Teri Mohabbat Mein",
+    artist: "Kumar Sanu|Sadhana Sargam",
+    img: "music-8",
+    src: "music-8"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
